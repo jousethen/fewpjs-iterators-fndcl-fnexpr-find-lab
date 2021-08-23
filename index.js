af6cbefed7694df80f503c33081559e474d082d3
jousethen <jthen93@gmail.com>
2021-08-23 14:09:50 -0400

    commit tree 5ef71f38d659725e0c28df8be43044ce39eceea0

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function superbowlWin(record) {
+  let hasWin = record.find(function (g) {
+    return g.result === "W"
+  });
 
+  if (hasWin) {
+    return hasWin.year;
+  } else {
+    return hasWin;
+  }
+}
